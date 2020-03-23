f3edd3debd0fcba1625755ba9584750c7db88c21
Anton-Joe <anton_yejx@hotmail.com>
2020-03-23 21:54:01 +0800

    Initial commit

---
+++ template_demo_for_in_标签/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+
+context = {
+    'Books': [
+        '三国演义',
+        '水浒传',
+        '西南大学'
+    ],
+    'Books_Info':[
+        {
+            'book_name':'桑国演义',
+            'author':'叶敬轩'
+        },
+        {
+            'book_name': '四国演义',
+            'author':'叶敬轩2'
+        },
+    ],
+    'keys':{
+        '第一个键': '1',
+        '第二个键': '2',
+        '第三个键': '3',
+    }
+}
+
+
+def index(request):
+    return render(request, 'index.html', context=context)

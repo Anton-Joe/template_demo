9c3a127db6ebc3ba4243e109e1e47681c25018c4
Anton-Joe <anton_yejx@hotmail.com>
2020-03-27 19:34:03 +0800

    @register.filer 快速注册过滤器

--- car/templatetags/my_filter.py
+++ car/templatetags/my_filter.py
@@ -3,9 +3,16 @@ from django import  template
 register = template.Library()
 
 
+# 第一种写法---------------------start
 def my_first_filter(value, word):
     return value + word
 
 
 register.filter('my_first_filter_name', my_first_filter)
+# -------------------------------end
 
+
+# 另外一种写法：
+# @register.filter
+# def my_first_filter(value, word):
+#     return value + word
